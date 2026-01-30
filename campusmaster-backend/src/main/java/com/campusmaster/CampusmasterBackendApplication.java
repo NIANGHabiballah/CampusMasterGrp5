0a1331a6194baf3755ce30c2c26f1e45b8a04b09
Tafsir HABY NIANG <tafsirhabyng27@gmail.com>
2026-01-30 11:59:07 +0000

    Installatin du nouveau back-end avec java

---
+++ campusmaster-backend/src/main/java/com/campusmaster/CampusmasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campusmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusmasterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusmasterBackendApplication.class, args);
+	}
+
+}
---
+++ campusmaster-backend/src/test/java/com/campusmaster/CampusmasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusmasterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
